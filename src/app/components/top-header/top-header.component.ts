4d31eafc5465aedb6c46b716c3efe9cf8cf71d3a
Syxfqn <msyaf0911@gmail.com>
2024-02-03 22:34:34 +0800

    Designed About page and link top header buttons to social media links

--- src/app/components/top-header/top-header.component.ts
+++ src/app/components/top-header/top-header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { socialMediaInterface } from '../../interface/social-media';
 
 @Component({
   selector: 'app-top-header',
@@ -6,9 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './top-header.component.scss'
 })
 export class TopHeaderComponent {
-  public readonly instagram = '@trhine_ig';
-  public readonly whatsapp = '+60-123456789';
-  public readonly tiktok = 'trhine_tiktok';
-  public readonly location = 'Damansara, Malaysia';
-  public readonly working_hrs = 'Monday - Friday, 8.00am - 9.00pm'
+  public readonly instagram: socialMediaInterface = {
+    name: '@trhine_ig',
+    link: 'https://www.instagram.com/taufiqrhine/'
+  };
+  public readonly whatsapp: socialMediaInterface = {
+    number: '+60-123456789',
+    link: 'https://wa.me/60182331065'
+  };
+  public readonly tiktok: socialMediaInterface = {
+    name: 'trhine_tiktok',
+    link: 'https://www.tiktok.com/@taufiqrhinetr'
+  };
+  public readonly location: socialMediaInterface = { 
+    name: 'Damansara, Malaysia',
+    link: 'https://maps.app.goo.gl/7AhAb3GmyCwp9uuh8'
+  };
+  public readonly working_hrs = 'Monday - Friday, 8.00am - 9.00pm';
+
+  public goTo(link: string): void {
+    window.open(link, '_blank');
+  }
 }
---
+++ src/app/interface/social-media.ts
@@ -0,0 +1,5 @@
+export interface socialMediaInterface {
+    name?: string;
+    number?: string;
+    link: string;
+}
